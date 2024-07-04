aaffece4711d6aa62da502de4314796f3cb51099
Zülal Cengiz <zc.swe@gmail.com>
2024-07-04 13:41:52 +0300

    @chakra-ui/theme-tools do not work

--- theme.js
+++ theme.js
@@ -1,21 +1,11 @@
 import { extendTheme } from "@chakra-ui/react";
-import { mode } from "@chakra-ui/theme-tools";
 
 const config = {
   initialColorMode: "dark",
   useSystemColorMode: false,
 };
 
-const styles = {
-  global: (props) => ({
-    body: {
-      bg: mode(
-        props.theme.semanticTokens.colors["chakra-body-bg"]._light,
-        "blackAlpha.900"
-      )(props),
-    },
-  }),
-};
+const styles = {};
 
 const theme = extendTheme({ config, styles });
 
