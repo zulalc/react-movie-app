effd57eea8feb0599d3e3a535bed16e563bc7eaf
Zülal Cengiz <zc.swe@gmail.com>
2024-07-05 23:47:31 +0300

    React-Router-and-Paths

--- movie-app/src/main.jsx
+++ movie-app/src/main.jsx
@@ -1,15 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import App from "./App.jsx";
 import "./index.css";
 import theme from "../../theme.js";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      //array of routes
+      {
+        path: "/",
+        element: <div>Home</div>,
+      },
+      {
+        path: "/movies",
+        element: <div>Movies</div>,
+      },
+      {
+        path: "/shows",
+        element: <div>TV Shows</div>,
+      },
+      {
+        path: "/search",
+        element: <div>Search</div>,
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-    <ChakraProvider theme={theme} cssVarsRoot="body">
-      <App />
+    <ChakraProvider theme={theme}>
+      <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>
 );
