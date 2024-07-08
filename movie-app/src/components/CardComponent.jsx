9619f26806f1addde41d6956a5ca524ae4006f70
Zülal Cengiz <zc.swe@gmail.com>
2024-07-08 21:42:27 +0300

    Card-component-Updates

--- movie-app/src/components/CardComponent.jsx
+++ movie-app/src/components/CardComponent.jsx
@@ -1,12 +1,56 @@
-import { Box, Image } from "@chakra-ui/react";
+import { Box, Flex, Image, Text } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import { imagePath } from "../services/api";
+import { StarIcon } from "@chakra-ui/icons";
 
 const CardComponent = ({ item }) => {
   return (
     <Link to="/">
-      <Box>
-        <Image src={`${imagePath}/${item?.poster_path}`} />
+      <Box
+        position={"relative"}
+        transform={"scale(1)"}
+        _hover={{
+          transform: { base: "scale(1)x", md: "scale(1.08)" }, //scale for tablet won't change
+          transition: "transform 0.2s ease-in-out", //0.2 seconds
+          zIndex: "10",
+          "& .overlay": {
+            opacity: 1,
+          },
+        }}
+      >
+        <Image
+          src={`${imagePath}/${item?.poster_path}`}
+          alt={item?.title || item?.name}
+          height={"100%"}
+        />
+        <Box
+          className="overlay"
+          position={"absolute"}
+          p={"2"}
+          bottom={"0"}
+          left={"0"}
+          w={"100%"}
+          h={"33%"}
+          bg={"rgba(0,0,0,0.9)"}
+          opacity={"0"}
+          transition={"opacity 0.3s ease-in-out"}
+        >
+          <Text textAlign={"center"}>{item?.title || item?.name}</Text>
+          <Text textAlign={"center"} fontSize={"small"} color={"#1679AB"}>
+            {new Date(
+              item?.release_date || item?.first_air_date
+            ).getFullYear() || "N/A"}
+          </Text>
+          <Flex
+            alignItems={"center"}
+            justifyContent={"center"}
+            gap={"2"}
+            mt={"4"}
+          >
+            <StarIcon fontSize={"small"} />
+            <Text>{item?.vote_average.toFixed(1)}</Text>
+          </Flex>
+        </Box>
       </Box>
     </Link>
   );
--- movie-app/src/pages/Home.jsx
+++ movie-app/src/pages/Home.jsx
@@ -1,4 +1,11 @@
-import { Box, Container, Flex, Grid, Heading } from "@chakra-ui/react";
+import {
+  Box,
+  Container,
+  Flex,
+  Grid,
+  Heading,
+  Skeleton,
+} from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { fetchTrending } from "../services/api";
 import CardComponent from "../components/CardComponent";
@@ -70,7 +77,13 @@ const Home = () => {
         gap="4"
       >
         {data &&
-          data?.map((item) => <CardComponent key={item?.id} item={item} />)}
+          data?.map((item, i) =>
+            loading ? ( //when loading there is a possibility we don't have data so we dont use data key here
+              <Skeleton height={300} key={i} />
+            ) : (
+              <CardComponent key={item?.id} item={item} />
+            )
+          )}
       </Grid>
     </Container>
   );
